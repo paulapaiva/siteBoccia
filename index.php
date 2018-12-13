867cd88178d1cf1f9746c909bcc3929426f90dac
Paula Paiva <paulapaivamoreira@yahoo.com>
2018-12-13 18:16:50 -0200

    Teste form

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
  <? 
 	if(isset($_POST['submit'])){
 		$to = "paulapaivamoreira@yahoo.com"; // this is your Email address
-		$from = $_POST['email']; // this is the sender's Email address
+		$from = "paulapaivamoreira@yahoo.com"; // this is the sender's Email address
 		$first_name = $_POST['first_name'];
-		$last_name = $_POST['last_name'];
 		$subject = "Form submission";
 		$message = $_POST['message']
-		$message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+		$message = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
 
 
 		$headers = "From:" . $from;
